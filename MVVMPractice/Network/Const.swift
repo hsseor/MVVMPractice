9fe967999e969c0bec8c3537ce922ecac4c39acb
hsseor <hsseor@naver.com>
2023-09-12 02:54:30 +0900

    [Feat] TV Entity 구현

---
+++ MVVMPractice/Network/Const.swift
@@ -0,0 +1,10 @@
+//
+//  Const.swift
+//  MVVMPractice
+//
+//  Created by 홍서린 on 2023/09/12.
+//
+
+import Foundation
+
+let APIKEY = "afd665a677d11ee5b20ba2120bafe250"
---
+++ MVVMPractice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVMPractice
+//
+//  Created by 홍서린 on 2023/09/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+/*
+ Kingfisher(이미지 쉽게 다루기/ downloading & caching images)
+ SnapKit (제약조건 설정 시 코드 줄이기)
+ Rxswift (버튼 클릭 등의 이벤트 처리)
+ RxAlamofire
+ */
